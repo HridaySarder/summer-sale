58526353dc48c8c461d21490196a01d562f123d3
HridaySarder <hridoyaug12@gmail.com>
2023-08-20 19:50:54 +0600

    added some js part

---
+++ js/index.js
@@ -0,0 +1,30 @@
+// function handleClickBtn(target){
+//   const selectedItem = document.getElementById('select-items');
+//   const itmeName = target.parentNode.childNodes[3].innerText;
+//   const li = document.createElement('li');
+//   li.innerText = itmeName;
+//   selectedItem.appendChild(li);
+//   console.log(itmeName);
+
+// }
+
+let itemCount = 1;
+let total = 0;
+const cardElements = document.querySelectorAll(".card");
+cardElements.forEach((card) => {
+  card.addEventListener("click", () => {
+    const selectedItem = document.getElementById("select-items");
+    const itemName = card.querySelector(".card-title").innerText;
+    const cardPrice = parseFloat(card.querySelector(".card-price").innerText);
+    const li = document.createElement("li");
+    li.innerText = `${itemCount}. ${itemName}`;
+    selectedItem.appendChild(li);
+    itemCount++;
+    total += cardPrice;
+    const totalPriceElement = document.getElementById("total-price-value");
+    totalPriceElement.textContent = total.toFixed(2) + " ";
+
+    // console.log(itemName);
+    // console.log('Total:', total.toFixed(2));
+  });
+});
